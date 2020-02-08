bddb888d2fbaf5b7f02278360c6da525650e9a43
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-08 17:50:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,19 +24,18 @@ def shift_with_args(array)
  array.shift(2)
 end
 
-def using_concat(a,b,c)
+def using_concat(a,b)
   a = ["abc", "xyz", "dog"] 
   
 # declaring array 
 b = ["cow", "cat", "dog"] 
   
-# declaring array 
-c = ["cat", "1", "dog"] 
+
   
 # COMBINING TWO ARRAYS 
 puts "combining a and b : #{a.concat(b)}\n\n"
   
 puts "combining c and b : #{b.concat(c)}\n\n"
   
-puts "combining a and c : #{c.concat(a)}\n\n"
+
 end
