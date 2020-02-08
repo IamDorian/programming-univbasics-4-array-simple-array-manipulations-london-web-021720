3959a7c0d24880684324423937c8a50315b159dc
Dorian Calin <dorin.m.calin@gmail.com>
2020-02-08 18:14:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,7 +24,7 @@ def shift_with_args(array)
  array.shift(2)
 end
 
-def using_concat(array)
+def using_concat(array,all_my_favs)
 all_my_favs = ["raindrops on roses", "whiskers on kittens", "sports cars", "ftaliron school"]
 array.concat(all_my_favs)
 end
